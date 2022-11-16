a39311f662f5968d15633aca8678a9c20d8fe0d4
fahiid33 <trey34809@gmail.com>
2022-11-16 02:21:08 +0100

    in progress

--- cpp-00/ex01/contact.hpp
+++ cpp-00/ex01/contact.hpp
@@ -1,31 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phone_book.hpp                                     :+:      :+:    :+:   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 17:31:37 by fstitou           #+#    #+#             */
-/*   Updated: 2022/11/16 01:13:41 by fstitou          ###   ########.fr       */
+/*   Created: 2022/11/10 23:06:00 by fstitou           #+#    #+#             */
+/*   Updated: 2022/11/16 01:19:30 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONE_BOOK_HPP
-# define PHONE_BOOK_HPP
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
 
-#include "contact.hpp"
 #include <iostream>
 
-class Phonebook
+
+class Contact
 {
-    private:
-    Contact contacts[8];
+	private:
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
     public:
-    void	set_details(int i);
-    void	display_contacts(int i);
-	int		is_strdigit(std::string& str);
-};  
-
-
+	void	add();
+	void	get_details();
+	void	print_contacts();
+	void	ft_infos(int index);
+};
 
 #endif
--- cpp-00/ex01/phone_book.cpp
+++ cpp-00/ex01/phone_book.cpp
@@ -6,9 +6,53 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:31:00 by fstitou           #+#    #+#             */
-/*   Updated: 2022/11/10 17:32:48 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/11/16 01:14:37 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phone_book.hpp"
 
+void    Contact::add()
+{
+    std::string input1;
+    std::cout << "First name :";
+    std::getline(std::cin, first_name);
+    std::cout << "Last name :";
+    std::getline(std::cin, last_name);
+    std::cout << "Nickname :";
+    std::getline(std::cin, nickname);
+    std::cout << "Phone number :";
+    std::getline(std::cin, phone_number);
+    std::cout << "Darkest secret :";
+    std::getline(std::cin, darkest_secret);
+}
+
+void    Phonebook::set_details(int index)
+{
+    contacts[index].add();
+}
+int main()
+{
+    Phonebook pb;
+    std::string input;
+    int i;
+    i = 0;
+    while (1)
+    {
+        std::cout << "please enter ADD | SEARCH | EXIT "<< std::endl;
+        std::getline(std::cin, input);
+        if (input == "ADD" )
+        {
+            pb.set_details(i);
+        }
+        else if (input == "SEARCH")
+        {
+            std::cout << "please enter an index :";
+            std::getline(std::cin, input);
+        }
+        else if (input == "EXIT")
+            exit(0);
+        i++;
+    }
+    
+}
