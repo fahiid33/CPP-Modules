0e443c52a8044ec1c0521c32f88cd9ee959692c9
fahiid33 <trey34809@gmail.com>
2022-11-18 03:40:16 +0100

    cpp00 finish

--- cpp-00/ex01/contact.hpp
+++ cpp-00/ex01/contact.hpp
@@ -6,7 +6,7 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 23:06:00 by fstitou           #+#    #+#             */
-/*   Updated: 2022/11/16 05:37:11 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/11/18 03:05:08 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,9 @@ class Contact
 	std::string phone_number;
 	std::string darkest_secret;
     public:
-	void	add();
+	int		add();
 	void	get_details();
-	void	print_contacts();
-	void	ft_infos(int index);
+	void	table_index(int index);
 };
 
 #endif
--- cpp-00/ex01/phone_book.cpp
+++ cpp-00/ex01/phone_book.cpp
@@ -6,25 +6,53 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:31:00 by fstitou           #+#    #+#             */
-/*   Updated: 2022/11/16 05:57:13 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/11/18 03:38:32 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phone_book.hpp"
+#include <sstream>
+#include <stdlib.h>
+#include <iomanip>
 
-void    Contact::add()
+int	Contact::add()
 {
+	std::string s, s1, s2, s3, s4;
 	std::cout << "First name :";
-	std::getline(std::cin, first_name);
+	std::getline(std::cin, s);
+	if (!std::cin)
+		exit(0);
 	std::cout << "Last name :";
-	std::getline(std::cin, last_name);
+	std::getline(std::cin, s1);
+	if (!std::cin)
+		exit(0);
 	std::cout << "Nickname :";
-	std::getline(std::cin, nickname);
+	std::getline(std::cin, s2);
+	if (!std::cin)
+		exit(0);
 	std::cout << "Phone number :";
-	std::getline(std::cin, phone_number);
+	std::getline(std::cin, s3);
+	if (!std::cin)
+		exit(0);
 	std::cout << "Darkest secret :";
-	std::getline(std::cin, darkest_secret);
+	std::getline(std::cin, s4);
+	if (!std::cin)
+		exit(0);
+	if (s.empty() && s1.empty() && s2.empty()
+			&& s3.empty() && s4.empty())
+	{
+		std::cout << std::setw(50);
+		std::cout<<"A saved contact can't have empty fields"<<std::endl;
+		return (0);
+	}
+	first_name = s;
+	last_name = s1;
+	nickname = s2;
+	phone_number = s3;
+	darkest_secret = s4;
+	return (1);
 }
+
 void	Contact::get_details()
 {
 	std::cout << "First name :"<< first_name<<std::endl;
@@ -34,45 +62,106 @@ void	Contact::get_details()
 	std::cout << "Darkest secret :"<< darkest_secret<<std::endl;
 }
 
-void    Contact::print_contacts()
+int	Phonebook::set_details(int index)
 {
-    // for (int j = 0; j < i; j++)
-    // {
-    // }
+	if (contacts[index].add())
+		return (1);
+	return (0);
 }
-void    Phonebook::set_details(int index)
+int	Phonebook::is_strdigit(std::string& str)
 {
-    contacts[index].add();
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return(1);
 }
-void	Phonebook::display_contacts(int insex)
+
+void	Contact::table_index(int index)
 {
-	contacts[insex].get_details();
+	std::string	s;
+	std::cout<< std::setw(10)<< index << "|";
+	s = first_name;
+	if (s.length() > 10)
+	{
+		s.resize(10);
+		s.back() = '.';
+	}
+	std::cout << std::setw(10) << s << "|";
+	s = last_name;
+	if (s.length() > 10)
+	{
+		s.resize(10);
+		s.back() = '.';
+	}
+	std::cout << std::setw(10) << s << "|";
+	s = nickname;
+	if (s.length() > 10)
+	{
+		s.resize(10);
+		s.back() = '.';
+	}
+	std::cout << std::setw(10) << s << std::endl;
+}
+
+void	Phonebook::display_contacts(int i)
+{
+	int indexx;
+	std::string index;
+	std::cout << std::setw(10) << "index";
+	std::cout << std::setw(10) << "|first name";
+	std::cout << std::setw(10) << "|last name";
+	std::cout << std::setw(10) << "|nickname" << std::endl;
+	for (int j = 0; j < i; j++)
+		contacts[j].table_index(j);
+	std::cout << "please enter an index :";
+	std::getline(std::cin, index);
+	if (!std::cin)
+		exit(0);
+	if (index.empty() || !is_strdigit(index))
+		std::cout <<std::setw(40)<< "index must be a positive number"<< std::endl;
+	else
+	{
+		indexx = std::stoi(index);
+		if (indexx >= i)
+			std::cout << std::setw(40) << "No informations try again "<< std::endl;
+		else
+			contacts[indexx].get_details();
+	}
 }
 int main()
 {
-    Phonebook pb;
-    std::string input;
-	int	index;
-    int i;
-    i = 0;
-    while (1)
-    {
-		input = "";
-        std::cout << "please enter ADD | SEARCH | EXIT "<< std::endl;
-        std::getline(std::cin, input);
-        if (input == "ADD")
-        {
-            pb.set_details(i);
-        }
-        else if (input == "SEARCH")
-        {
-            std::cout << "please enter an index :";
-            std::cin>> index;
-			pb.display_contacts(index);	
-        }
-        else if (input == "EXIT" || !std::cin)
-            exit(0);
-		// if (std::cin && (input == "ADD" || input == "SEARCH"))
-    }
-    
+	Phonebook	pb;
+	std::string	input;
+	int			i;
+	int			k;
+
+	i = 0;
+	k = 0;
+	while (1)
+	{
+		std::cout << "please enter ADD | SEARCH | EXIT "<< std::endl;
+		std::getline(std::cin, input);
+		if (input == "ADD")
+		{
+			if (k == 8)
+				i = 0;
+			if (pb.set_details(i))
+			{
+				i++;
+				if (k != 8)
+					k++;
+			}
+		}
+		else if (input == "SEARCH")
+			pb.display_contacts(k);
+		else if (input == "EXIT" || !std::cin)
+			exit(0);
+	}
+
 }
