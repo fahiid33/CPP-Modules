642632cb5758ff6bae3da8b28c0f15e4bbdc4eef
fahiid33 <trey34809@gmail.com>
2022-12-11 20:20:35 +0100

    cpp01/ex05

--- cpp-01/ex03/HumanB.cpp
+++ cpp-01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 00:52:47 by fstitou           #+#    #+#             */
-/*   Updated: 2022/12/07 20:35:49 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/12/11 18:52:18 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ HumanB::HumanB(std::string name)
 void    HumanB::attack(void)
 {
     std::cout << w_name << " attacks with their " << weap->getType() << std::endl;
-
 }
 void    HumanB::setWeapon(Weapon &wp)
 {
