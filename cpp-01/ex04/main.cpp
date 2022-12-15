421829c31f7824e5aa0b0d9b41baecf05b5347cd
fahiid33 <trey34809@gmail.com>
2022-12-15 23:21:55 +0100

    fixed ex04

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 18:53:12 by fstitou           #+#    #+#             */
-/*   Updated: 2022/12/11 20:19:36 by fstitou          ###   ########.fr       */
+/*   Updated: 2022/12/15 23:20:22 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 
 void	search_and_replace(std::string line, std::string s1, std::string s2)
 {
-	int			pos;
-	int			s1_len;
-	int			line_len;	
+	int pos;
 	std::string ret;
+	int s1_len;
+	int line_len;
 
 	s1_len = s1.length();
-	while ( (pos = line.find(s1)) != -1)
+	while ( (pos = line.find(s1)) != -1 )
 	{
 		line_len = line.length();
-		ret = line.substr(0, pos);
+		ret =  ret + line.substr(0,pos);
 		ret = ret + s2;
-		line =  line.substr(pos + s1_len, line_len);
+		line = line.substr(pos + s1_len,line_len);
 	}
 	ret = ret + line;
-	// std::cout << ret << std::endl;
+	std::cout << "ret:  " << ret << std::endl;
 	std::ofstream ofs("replace.txt", std::ofstream::out);
 	ofs << ret;
 	ofs.close();
@@ -37,29 +37,35 @@ void	search_and_replace(std::string line, std::string s1, std::string s2)
 
 int main(int ac, char **av)
 {
-    std::string line;
-    std::string real_line;
-
-    if (ac == 4)
-    {
-        std::ifstream ifs(av[1]);
-        if (ifs.is_open())
-        {
-            while (getline(ifs, line))
-                real_line = real_line + line + '\n';
-            if (!line[0])
-                std::cout << "file is empty" << std::endl;
-            real_line.resize(real_line.size() - 1);
-            ifs.close();
-        }
-        else
-        {
-            std::cout << "Error : failed to open file" << std::endl;
-            return (0);
-        }
-        search_and_replace(real_line, av[2], av[3]);
-    }
-    else
+	std::string line;
+	std::string real_line;
+	int		j = 0;
+	if (ac == 4)
+	{
+		std::ifstream ifs(av[1]);
+		if (ifs.is_open())
+		{
+			while (getline(ifs, line))
+			{
+				real_line = real_line + line + '\n';
+				j++;
+			}
+			if (!line[0] && !j)
+			{
+				std::cout << "file is empty" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+			real_line.resize(real_line.size() - 1);
+			ifs.close();
+		}
+		else
+		{
+			std::cout << "Error : failed to open file" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		search_and_replace(real_line, av[2], av[3]);
+	}
+	else
 		std::cout << "error in parameters, USAGE : ./a.out <filename> <s1> <s2>" << std::endl;
 	return (0);
 }
