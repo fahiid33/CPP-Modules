09885b9fcad4fb9b4035ce1fd936311384edf929
fahiid33 <trey34809@gmail.com>
2023-01-11 23:30:06 +0100

    added cpp-08/ex00

--- cpp-08/ex00/easyfind.hpp
+++ cpp-08/ex00/easyfind.hpp
@@ -6,19 +6,25 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 03:47:47 by fstitou           #+#    #+#             */
-/*   Updated: 2023/01/11 03:51:48 by fstitou          ###   ########.fr       */
+/*   Updated: 2023/01/11 23:27:31 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <list>
 
 template <typename T>
 void    easyfind(T type, int to_find)
 {
-    
+    typename T::iterator it = std::find(type.begin(), type.end(), to_find);
+    if (it == type.end())
+        throw std::exception();
+    std::cout << "Found " << to_find << " at index " << std::distance(type.begin(), it) << std::endl;
 }
 
 #endif
--- cpp-08/ex00/main.cpp
+++ cpp-08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 03:48:24 by fstitou           #+#    #+#             */
-/*   Updated: 2023/01/11 03:48:38 by fstitou          ###   ########.fr       */
+/*   Updated: 2023/01/11 23:28:52 by fstitou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,34 @@
 
 int main()
 {
-    
+    std::vector<int> v;
+    for (int i = 0; i < 10; i++)
+        v.push_back(10 * i);
+    for (int k = 0; k < (int)v.size(); k++)
+        std::cout << "v["<< k << "] = " << v[k] << " ";
+    std::cout << std::endl;
+    try
+    {
+        easyfind(v, 50);
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Not found" << std::endl;
+    }
+    try
+    {
+        easyfind(v, 80);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "Not found" << std::endl;
+    }
+    try
+    {
+        easyfind(v, 100);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "Not found" << std::endl;
+    }
 }
