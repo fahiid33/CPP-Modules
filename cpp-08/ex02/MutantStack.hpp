08eb5ede8c6e4ffffb3ad8e52a0f39db5d8d9ece
fahiid33 <trey34809@gmail.com>
2023-01-12 19:58:17 +0100

    Added ex02

---
+++ cpp-08/ex02/MutantStack.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 19:29:27 by fstitou           #+#    #+#             */
+/*   Updated: 2023/01/12 19:50:57 by fstitou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+#include <list>
+#include <iostream>
+#include <iterator>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack(){}
+        MutantStack(MutantStack const & src) 
+        {
+            *this = src;
+        };
+        ~MutantStack() {}
+
+        MutantStack &operator=(MutantStack const &rhs)
+        {
+            if (this != &rhs) { }
+            return *this;
+        };
+
+        typedef typename std::deque<T>::iterator iterator;
+
+        iterator begin() {
+            return this->c.begin();
+        };
+
+        iterator end() {
+            return this->c.end();
+        };
+};
+
+#endif
